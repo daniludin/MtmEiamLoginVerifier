bc7c899f54aff00e7dd4dff065dbc9b20052b9ce
daniludin <daniel.ludin@me.com>
2023-12-17 13:14:19 +0100

    initial commit

---
+++ src/ch/bgs/anttask/MtmEiamLoginVerifier.java
@@ -0,0 +1,100 @@
+package ch.bgs.anttask;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.Task;
+
+//java.util.zip.ZipInputStream;
+
+public class MtmEiamLoginVerifier extends Task {
+
+	private String searchItem;
+
+	@Override
+	public void execute() {
+
+		if (getSearchItem() == null || getSearchItem().length() == 0) {
+			log("Das Attribut 'searchItem' fehlt oder ist leer.", Project.MSG_ERR);
+			return;
+		}
+		String fileZip = getProject().getProperty("dist.home") + "/" + getProject().getProperty("app.name") + "-" + getProject().getProperty("app.version") + ".war";
+
+		try {
+			byte[] buffer = new byte[1024];
+			ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip));
+			ZipEntry zipEntry = zis.getNextEntry();
+			while (zipEntry != null) {
+				// System.out.println("zipEntry: " + zipEntry.getName());
+
+				if (zipEntry.getName().contains("applicationContext.xml")) {
+
+					int read = 0;
+					StringBuilder s = new StringBuilder();
+
+					while ((read = zis.read(buffer, 0, 1024)) >= 0) {
+						s.append(new String(buffer, 0, read));
+					}
+
+					Pattern pattern = Pattern.compile("<!--[\\s\\S\\n]*?-->");
+
+					Matcher matcher = pattern.matcher(s.toString());
+					while (matcher.find()) {
+						if (matcher.group().contains("saml")) {
+							log("*********************************************************", Project.MSG_ERR);
+							log("ALARM! Der Eintrag, der den Suchbegriff  '" + getSearchItem() + "' enthält, ist auskommentiert! ", Project.MSG_ERR);
+							log("\r\n", Project.MSG_ERR);
+							log(matcher.group(), Project.MSG_ERR);
+							log("\r\n", Project.MSG_ERR);
+							log("*********************************************************", Project.MSG_ERR);
+						}
+					}
+
+				}
+				zipEntry = zis.getNextEntry();
+			}
+
+			zis.closeEntry();
+			zis.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			log("FileNotFoundException : " + e.getMessage(), Project.MSG_ERR);
+
+		} catch (IOException e) {
+			log("IOException : " + e.getMessage(), Project.MSG_ERR);
+		}
+
+	}
+
+	public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+
+		File destFile = new File(destinationDir, zipEntry.getName());
+
+		String destDirPath = destinationDir.getCanonicalPath();
+		String destFilePath = destFile.getCanonicalPath();
+
+		if (!destFilePath.startsWith(destDirPath + File.separator)) {
+			throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+		}
+
+		return destFile;
+	}
+
+	public String getSearchItem() {
+
+		return this.searchItem;
+	}
+
+	public void setSearchItem(String searchItem) {
+
+		this.searchItem = searchItem;
+	}
+
+}
---
+++ src/ch/bgs/anttask/MtmEiamLoginVerifierTest.java
@@ -0,0 +1,76 @@
+package ch.bgs.anttask;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class MtmEiamLoginVerifierTest {
+
+	public static void main(String[] args) {
+
+		new MtmEiamLoginVerifierTest().test();
+	}
+
+	public void test() {
+
+		String fileZip = "C:/Users/danie/git/substidoc/dist/substidoc-0.0.0.war";
+		File destDir = new File("src/main/resources/unzipTest");
+
+		try {
+			byte[] buffer = new byte[1024];
+			ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip));
+			ZipEntry zipEntry = zis.getNextEntry();
+			while (zipEntry != null) {
+				// System.out.println("zipEntry: " + zipEntry.getName());
+
+				if (zipEntry.getName().contains("applicationContext.xml")) {
+
+					int read = 0;
+					StringBuilder s = new StringBuilder();
+
+					while ((read = zis.read(buffer, 0, 1024)) >= 0) {
+						s.append(new String(buffer, 0, read));
+					}
+					// System.out.println("content: " + s.toString());
+
+					// <!--[\s\S\n]*?-->
+
+					Pattern pattern = Pattern.compile("<!--[\\s\\S\\n]*?-->");
+
+					// in case you would like to ignore case sensitivity,
+					// you could use this statement:
+					// Pattern pattern = Pattern.compile("\\s+", Pattern.CASE_INSENSITIVE);
+					Matcher matcher = pattern.matcher(s.toString());
+					// check all occurance
+					while (matcher.find()) {
+						// System.out.print("Start index: " + matcher.start());
+						// System.out.print(" End index: " + matcher.end() + " ");
+						System.out.println(matcher.group());
+						if (matcher.group().contains("saml")) {
+							System.err.println("ALARM!!!");
+						}
+					}
+
+				}
+				zipEntry = zis.getNextEntry();
+			}
+
+			zis.closeEntry();
+			zis.close();
+		} catch (
+
+		FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
